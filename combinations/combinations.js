859b218a94c2b37d04776a5fa05a4d4886ce427b
Innocent Zvovuranda <innocent@digidna.co.za>
2021-01-18 11:51:06 +0200

    added some referencies

--- combinations/combinations.js
+++ combinations/combinations.js
@@ -2,6 +2,7 @@
 // Time complexity 2^n
 // Space O(n^2)
 // NB* understand tree visualiazation
+// Ref:  https://www.youtube.com/watch?v=NA2Oj9xqaZQ, https://www.youtube.com/watch?v=wRH2I6IN4BE&t=1s
 const combinations = (elements) => {
   if (elements.length === 0) return [[]];
   const cur = elements[0];
